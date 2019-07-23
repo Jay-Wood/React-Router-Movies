112e63da8e139b54f6b2b1ec763d9f38c4ec73ef
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-07-23 15:55:11 -0700

    Added one stretch goal (DRY refactor)

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
+import MovieCard from "./MovieCard.js";
 import axios from 'axios';
 
 const MovieList = props => {
@@ -29,26 +30,10 @@ const MovieList = props => {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, id, metascore, stars } = movie;
   return (
-    <Link to={`/movies/${id}`}>
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
+      <div>
+        <MovieCard key={movie.id} movie={movie} />
       </div>
-    </Link>
   );
 }
 
